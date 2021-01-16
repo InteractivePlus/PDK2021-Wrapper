b022d432193874e35a475d4d134fbcc4de26df2c
Windy Feng <1745500559@qq.com>
2021-01-16 15:56:55 +0800

    Add Compression in MySQL Impl Logs.

--- src/Implementions/MySQL/LoggerStorageMySQLImpl.php
+++ src/Implementions/MySQL/LoggerStorageMySQLImpl.php
@@ -64,7 +64,7 @@ class LoggerStorageMySQLImpl extends LoggerStorage implements MySQLStorageImpl{
             'operation_success' => $logEntity->success ? 1 : 0,
             'pdk_exception_code' => $logEntity->PDKExceptionCode,
             'client_addr' => $logEntity->getClientAddr(),
-            'log_context' => empty($logEntity->getContexts()) ? null : json_encode($logEntity->getContexts())
+            'log_context' => empty($logEntity->getContexts()) ? null : gzcompress(json_encode($logEntity->getContexts()))
         );
         $addRst = $this->db->insert('logs',$dataToAdd);
         if(!$addRst){
@@ -80,6 +80,21 @@ class LoggerStorageMySQLImpl extends LoggerStorage implements MySQLStorageImpl{
         }
         $this->db->where('log_level',$highestLogLevel,'<=');
         $this->db->delete('logs');
+    }
+    protected function LogEntityFromDataRow(array $dataRow) : LogEntity{
+        $newEntity = new LogEntity(
+            $dataRow['action_id'],
+            empty($dataRow['app_uid']) ? APPSystemConstants::NO_APP_RELATED_APPUID : $dataRow['app_uid'],
+            empty($dataRow['user_uid']) ? UserSystemConstants::NO_USER_RELATED_UID : $dataRow['user_uid'],
+            $dataRow['log_time'],
+            $dataRow['log_level'],
+            $dataRow['operation_success'] == 1,
+            $dataRow['pdk_exception_code'],
+            empty($dataRow['client_addr']) ? null : $dataRow['client_addr'],
+            empty($dataRow['log_message']) ? null : $dataRow['log_message'],
+            empty($dataRow['log_context']) ? null : gzuncompress($dataRow['log_context'])
+        );
+        return $newEntity;
     }
     public function getLogItemsBetween(int $fromTime = -1, int $toTime = -1, int $offset = 0, int $count = -1, int $lowestLogLevel = PDKLogLevel::NOTICE) : MultipleResult{
         if($fromTime > 0){
@@ -97,9 +112,13 @@ class LoggerStorageMySQLImpl extends LoggerStorage implements MySQLStorageImpl{
         if(!$result){
             throw new PDKStorageEngineError('failed to fetch data from database',MySQLErrorParams::paramsFromMySQLiDBObject($this->db));
         }
+        $resultOBJArray = array();
+        foreach ($result as $singleDataRow){
+            $resultOBJArray[] = $this->LogEntityFromDataRow($singleDataRow);
+        }
         $returnResult = new MultipleResult(
             $this->db->count,
-            $result,
+            $resultOBJArray,
             $this->db->totalCount,
             $count > 0 ? $offset : 0
         );
@@ -116,4 +135,5 @@ class LoggerStorageMySQLImpl extends LoggerStorage implements MySQLStorageImpl{
         $count = $this->db->getValue('logs','count(*)');
         return $count;
     }
+    
 }
