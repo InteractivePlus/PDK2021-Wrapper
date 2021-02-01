1fda0a072c6bcc142e1244a97d7c95ab2683589c
Windy Feng <1745500559@qq.com>
2021-02-01 20:12:33 +0800

    Update Config_template.php

--- src/Config_template.php
+++ src/Config_template.php
@@ -1,6 +1,10 @@
 <?php
 namespace InteractivePlus\PDK2021;
 
+use InteractivePlus\PDK2021Core\APP\APPSystemFormatSetting;
+use InteractivePlus\PDK2021Core\APP\APPSystemFormatSettingImpl;
+use InteractivePlus\PDK2021Core\User\Setting\SettingBoolean;
+use InteractivePlus\PDK2021Core\User\Setting\UserSetting;
 use InteractivePlus\PDK2021Core\User\UserSystemFormatSettingImpl;
 
 class Config{
@@ -20,13 +24,17 @@ class Config{
     public string $DXTON_APISecret = '';
     
     public UserSystemFormatSettingImpl $USER_SYSTEM_CONSTRAINTS;
+    public UserSetting $USER_SYSTEM_DEFAULT_SETTINGS;
+
+    public APPSystemFormatSetting $APP_SYSTEM_FORMAT_CONSTRAINTS;
+
     public bool $SLIM_CHECK_PROXY = false;
     public ?array $SLIM_PROXY_IPS = null;
 
     public int $VERICODE_AVAILABLE_DURATION = 1200;
     public int $TOKEN_AVAILABLE_DURATION = 3600 * 24 * 1;
     public int $REFRESH_TOKEN_AVAILABLE_DURATION = 3600 * 24 * 10;
-
+    
     public int $CAPTCHA_AVAILABLE_DURATION = 60 * 5;
     public int $CAPTCHA_PHRASE_LEN = 5;
 
@@ -45,6 +53,18 @@ class Config{
             0,
             60,
             null
+        );
+        $this->USER_SYSTEM_DEFAULT_SETTINGS = new UserSetting(
+            SettingBoolean::SET_YES,
+            SettingBoolean::SET_YES,
+            SettingBoolean::SET_YES,
+            SettingBoolean::SET_NO,
+            SettingBoolean::SET_YES,
+            SettingBoolean::SET_NO
+        );
+        $this->APP_SYSTEM_FORMAT_CONSTRAINTS = new APPSystemFormatSettingImpl(
+            0,
+            25
         );
     }
 }
